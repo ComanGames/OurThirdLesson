ec97913545ba57ae041603a8857be7d8c1b17843
ComanGames <alfaa.gen@gmail.com>
2016-04-09 20:48:38 +0300

    some changes

--- OurThirdLesson/Program.cs
+++ OurThirdLesson/Program.cs
@@ -11,15 +11,20 @@ namespace OurThirdLesson
         static void Main(string[] args)
         {
             string s = "10";
-            int a = 10;
+            int a = 10500;
             //int to string
             s = a.ToString();
             //string to int
             a = Int32.Parse(s);
+            Console.WriteLine(s.Remove(3));
+            Console.ReadKey();
+
             while (true)
             {
                 Console.WriteLine("Write your number");
-
+                string text = Console.ReadLine();
+                if (text == "q")
+                    break;
             }
 
         }
