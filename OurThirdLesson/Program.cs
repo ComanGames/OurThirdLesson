7113e80b43625e9a909902c10298f7a9cfda07eb
ComanGames <alfaa.gen@gmail.com>
2016-04-09 21:14:48 +0300

    Using void and non void

--- OurThirdLesson/Program.cs
+++ OurThirdLesson/Program.cs
@@ -3,30 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace OurThirdLesson
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string s = "10";
-            int a = 10500;
-            //int to string
-            s = a.ToString();
-            //string to int
-            a = Int32.Parse(s);
-            Console.WriteLine(s.Remove(3));
-            Console.ReadKey();
 
-            while (true)
-            {
-                Console.WriteLine("Write your number");
-                string text = Console.ReadLine();
-                if (text == "q")
-                    break;
-            }
+            WriteLine("Formula 1");
+            for (int i = 0; i <=10; i++)
+                WriteLine($"a{i}=\t{Formula1(i)}");
+
+            WriteLine("Formula2");
+            for (int i = 0; i <= 10; i++)
+                Formula2(i);
+
+            ReadKey();
+
 
         }
+
+        private static void Formula2(int n)
+        {
+            double a = (Math.Pow(-1, n)/(Math.Pow(n, 2)));
+            WriteLine($"a{n}=\t{a}");
+        }
+
+        static int Formula1(int n)
+        {
+
+            return (2*n) - 7;
+
+        }
+
     }
 }
